77a4bcaa93452283c08d010b0e910d33150b9ebf
vincenzorallo102@gmail.com <vincenzorallo102@gmail.com>
2025-06-06 10:32:16 +0200

    Added shell file for mixto

---
+++ Es2.sh
@@ -0,0 +1,11 @@
+#mixto Compilation
+export PATH=/usr/local/cuda-11.4/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH
+
+nvidia-smi
+
+nvcc filter.cu -o filter \
+  -I/usr/include/opencv4 \
+  -L/usr/lib/aarch64-linux-gnu \
+  -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc \
+  -lstdc++ -lcudart
