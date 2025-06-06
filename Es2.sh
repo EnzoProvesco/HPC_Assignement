0f572d007c6e6028a79d49db82ca46f754da500b
vincenzorallo102@gmail.com <vincenzorallo102@gmail.com>
2025-06-06 10:34:28 +0200

    Aggiustato pt2

--- Es2.sh
+++ Es2.sh
@@ -4,8 +4,10 @@ export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH
 
 nvidia-smi
 
-nvcc filter.cu -o filter \
+nvcc Es2.cpp -o Es2 \
   -I/usr/include/opencv4 \
   -L/usr/lib/aarch64-linux-gnu \
   -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc \
   -lstdc++ -lcudart
+
+./Es2
