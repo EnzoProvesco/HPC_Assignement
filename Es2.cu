9ee16ff4998046594e4d1c79f51366303557eecc
vincenzorallo102@gmail.com <vincenzorallo102@gmail.com>
2025-07-08 18:45:28 +0200

    Fix image processing argument handling and enhance profiling output in script

--- Es2.cu
+++ Es2.cu
@@ -282,12 +282,11 @@ int main(int argc, char** argv) {
     
         logFile << argv[1] <<";";
         //std::cout << "Processing image: " << argv[1] << std::endl;
-        cv::Mat result = GetResult(argv[i], logFile);
+        cv::Mat result = GetResult(argv[1], logFile);
         //std::cout << "Saving result to: " << argv[2] << std::endl;        
         if (if argc > 3 && argv[3] == std::string("-s")) {
             cv::imwrite(argv[2], result);
             std::cout << "Result saved successfully image"<< argv[1] << std::endl;
-
         }
 
         logFile <<"\n";
