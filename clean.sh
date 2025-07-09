dc6dba4be85c7ffb2ee6f32c6435bbc3141a79db
vincenzorallo102@gmail.com <vincenzorallo102@gmail.com>
2025-07-09 10:51:49 +0200

    Fixed input and output and log outside from github

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@ echo "Are you sure you want to delete all log files? (y/n)"
 read -r confirm
 if [ "$confirm" = "y" ]; then
     echo Deleting all files...
-    rm -rf logData/*
+    rm -rf ../logData/*
     echo All files deleted.
 else
     echo Operation cancelled.
